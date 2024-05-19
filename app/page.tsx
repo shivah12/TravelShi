2451b0ee6d12f844b078e3e0de3c69a0af7d9b3f
Shreeti Mohapatra <114981873+shivah12@users.noreply.github.com>
2024-05-19 12:02:09 +0530

    add

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Camp, Features, GetApp, Guide, Hero } from "@/components";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
+    </>
+  );
+}
---
+++ components/index.ts
@@ -0,0 +1,17 @@
+import Camp from "./Camp";
+import Features from "./Features";
+import Footer from "./Footer";
+import GetApp from "./GetApp";
+import Guide from "./Guide";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+
+export {
+    Camp,
+    Features,
+    Footer,
+    GetApp,
+    Guide,
+    Hero,
+    Navbar,
+};
